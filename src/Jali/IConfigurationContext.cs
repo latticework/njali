21de6dbce2c3034438935b89aa171e34e4ccf41f
Kenneth Brubaker <kennethbrubaker@outlook.com>
2015-09-18 23:10:32 -0500

    Setting up intial projects

---
+++ src/Jali/IConfigurationContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jali
+{
+    public interface IConfigurationContext
+    {
+    }
+}
