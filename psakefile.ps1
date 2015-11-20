63cf93821b43fb8fede9f2d3776b30714dc5ed89
Kenneth Brubaker <kennethbrubaker@outlook.com>
2015-11-20 10:00:22 -0600

    Set version to prealpha603 since it is a smaller change

--- psakefile.ps1
+++ psakefile.ps1
@@ -3,7 +3,7 @@ properties {
   $zipFileName = "Jali00r1.zip"
   $majorVersion = "0.0"
   $majorWithReleaseVersion = "0.0.1"
-  $nugetPrelease = "prealpha7"
+  $nugetPrelease = "prealpha603"
   $version = GetVersion $majorWithReleaseVersion
   $signAssemblies = $false
 #  $signKeyPath = "C:\Development\Releases\newtonsoft.snk"
